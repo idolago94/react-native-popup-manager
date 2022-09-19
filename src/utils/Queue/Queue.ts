56c33fca647f949f6a1c832744cca8e2b612a6b6
Ido Lago <idolago94@gmail.com>
2022-09-19 19:35:04 +0300

    first commit

---
+++ src/utils/Queue/Queue.ts
@@ -0,0 +1,36 @@
+import type { Element } from "./QueueTypes";
+
+interface Queue {
+  elements: Element
+  head: number
+  tail: number
+}
+
+class Queue {
+  constructor() {
+    this.elements = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(element: any) {
+    this.elements[this.tail] = element;
+    this.tail++;
+  }
+  dequeue() {
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
+  }
+  peek() {
+    return this.elements[this.head];
+  }
+  get length() {
+    return this.tail - this.head;
+  }
+  get isEmpty() {
+    return this.length === 0;
+  }
+}
+
+export default Queue;
